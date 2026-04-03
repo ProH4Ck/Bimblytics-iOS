7424e7f07f00892c4a9bc973935ea280d2c9baea
Emanuele Curati <ema@outlook.com>
2026-04-03 21:21:07 +0200

    Initial Commit

---
+++ Bimblytics/BimblyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BimblyticsApp.swift
+//  Bimblytics
+//
+//  Created by Emanuele Curati on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct BimblyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
