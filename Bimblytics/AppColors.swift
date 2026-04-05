34e784fb4d3ab681f7def738ec0fad260a1b1ca2
Emanuele Curati <ema@outlook.com>
2026-04-05 18:15:29 +0200

    Add colors, app icon & vector images

---
+++ Bimblytics/AppColors.swift
@@ -0,0 +1,21 @@
+//
+//  AppColors.swift
+//  Bimblytics
+//
+//  Created by Emanuele Curati on 05/04/2026.
+//
+
+
+import SwiftUI
+
+enum AppColors {
+    static let primary = Color("AppPrimary")
+    static let secondary = Color("AppSecondary")
+    static let accent = Color("AppAccent")
+    static let background = Color("AppBackground")
+    static let surface = Color("AppSurface")
+    static let textPrimary = Color("AppTextPrimary")
+    static let textSecondary = Color("AppTextSecondary")
+    static let colorMale = Color("AppColorMale")
+    static let colorFemale = Color("AppColorFemale")
+}
